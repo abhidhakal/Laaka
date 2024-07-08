dd21b179991f4dcd25c7b8ecb257fe4441538df6
Abhinav Dhakal <128499381+abhidhakal@users.noreply.github.com>
2024-07-08 20:01:30 +0545

    add

--- website/src/components/staticComponents/Footer.tsx
+++ website/src/components/staticComponents/Footer.tsx
@@ -21,11 +21,21 @@ const Footer: React.FC = () => {
                         <li><a href="#">Privacy Policy</a></li>
                     </ul>
                 </div>
+
                 <div className="footer-section contact">
                     <h2 className="logo-text">Contact Us</h2>
                     <p className="texts">Email: laakanepal@gmail.com</p>
                     <p className="texts">Phone: +977 9865206747</p>
                 </div>
+                <div className="footer-section socials">
+                    <h2>Our Socials</h2>
+                    <ul className="social-links">
+                        <a href="#" className="fa fa-facebook"></a>
+                        <a href="#" className="fa fa-twitter"></a>
+                        <a href="https://www.instagram.com/laaka.nepal/" target="_blank" className="fa fa-instagram"></a>
+                    </ul>
+                </div>
+
             </div>
             <div className="footer-bottom">
                 &copy; 2024 Laaka | All Rights Reserved
